bdaff5e798c52b7d27e1c61aba2166c5ca53a4bd
yangfc <uestc2803305011@gmaill.com>
2018-07-27 19:12:55 +0800

    init commit

--- 09_exp.py
+++ 09_exp.py
@@ -244,4 +244,35 @@ with tf.Session() as sess:
 
 print("Best Theta:")
 print(best_theta)
+
+###===============Using a momentum optimizer==========================================
+
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
     
+    best_theta = theta.eval()
+
+print("Best Theta:")
+print(best_theta)
