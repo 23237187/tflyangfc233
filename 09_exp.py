d277d7220c1ba3ffad1a33fade358ea96a026703
yangfc <uestc2803305011@gmaill.com>
2018-07-31 20:10:26 +0800

    save and restore

--- 09_exp.py
+++ 09_exp.py
@@ -276,3 +276,156 @@ with tf.Session() as sess:
 
 print("Best Theta:")
 print(best_theta)
+
+##===================Feeding Data========================================
+reset_graph()
+
+A = tf.placeholder(tf.float32, shape=(None, 3))
+B = A + 5
+with tf.Session() as sess:
+    B_val_1 = B.eval(feed_dict={A: [[1, 2, 3]]})
+    B_val_2 = B.eval(feed_dict={A:[[4,5,6],[7,8,9]]})
+
+print(B_val_1)
+
+print(B_val_2)
+
+##====================Mini Batch Gradient Descent==========================
+n_epochs = 1000
+learning_rate = 0.01
+
+reset_graph()
+
+X = tf.placeholder(tf.float32,shape=(None, n+1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+
+theta = tf.Variable(tf.random_uniform([n+1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+n_epochs = 10
+
+batch_size = 100
+n_bathches = int(np.ceil(m / batch_size))
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_bathches + batch_index)
+    indices = np.random.randint(m, size=batch_size)
+    X_batch = scaled_housing_data_plus_bias[indices]
+    y_batch = housing.target.reshape(-1, 1)[indices]
+    return X_batch, y_batch
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_bathches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+    
+    best_theta = theta.eval()
+
+best_theta
+
+#==================Saving and restoring a model========================================
+
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n+1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+            save_path = saver.save(sess, "./model/my_model.ckpt")
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+    save_path = saver.save(sess, "./model/my_model_final.ckpt")
+
+best_theta
+
+
+with tf.Session() as sess:
+    saver.restore(sess, "./model/my_model_final.ckpt")
+    best_theta_restored = theta.eval()
+
+np.allclose(best_theta, best_theta_restored)
+
+#==================retore graph==========================
+
+reset_graph()
+
+saver = tf.train.import_meta_graph("./model/my_model_final.ckpt.meta")
+theta = tf.get_default_graph().get_tensor_by_name("theta:0")
+
+with tf.Session() as sess:
+    saver.restore(sess, "./model/my_model_final.ckpt")
+    best_theta_restored = theta.eval()
+
+
+np.allclose(best_theta, best_theta_restored)
+
+
+#=========================Visualizing the Graph==============================
+##========================Inside Jupyter=====================================
+
+from IPython.display import clear_output, Image, display, HTML
+
+def strip_consts(graph_def, max_const_size=32):
+    """Strip large constant values from graph_def."""
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add() 
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = b"<stripped %d bytes>"%size
+    return strip_def
+
+def show_graph(graph_def, max_const_size=32):
+    """Visualize TensorFlow graph."""
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+
+    iframe = """
+        <iframe seamless style="width:1200px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
+
+show_graph(tf.get_default_graph())
