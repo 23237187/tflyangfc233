d89d58fd0a2a964dd140e36c0714a35ce2182284
yangfc <uestc2803305011@gmaill.com>
2018-08-31 20:10:05 +0800

    20180831

--- 10.py
+++ 10.py
@@ -160,6 +160,91 @@ feature_cols = [tf.feature_column.numeric_column("X", shape=[28 * 28])]
 dnn_clf = tf.estimator.DNNClassifier(hidden_units=[300, 100], n_classes=10, 
                                         feature_columns=feature_cols)
 input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"X":X_train}, y=y_train, num_epoch=40, batch_size=50, shuffle=True
+    x={"X":X_train}, y=y_train, num_epochs=40, batch_size=50, shuffle=True
 )
 dnn_clf.train(input_fn=input_fn)
+
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"X": X_test}, y=y_test, shuffle=False
+)
+eval_results = dnn_clf.evaluate(input_fn=test_input_fn)
+
+eval_results
+
+y_pred_iter = dnn_clf.predict(input_fn=test_input_fn)
+y_pred = list(y_pred_iter)
+y_pred[0]
+
+##===============================Using plian Tensorflow===========================================
+import tensorflow as tf
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+n_hidden2 = 100
+n_outputs = 10
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+def neuron_layer(X, n_neurons, name, activation=None):
+    with tf.name_scope(name):
+        n_inputs = int(X.get_shape()[1])
+        stddev = 2 / np.sqrt(n_inputs)
+        init = tf.truncated_normal((n_inputs, n_neurons), stddev=stddev)
+        W = tf.Variable(init, name="kernel")
+        b = tf.Variable(tf.zeros([n_neurons]), name="bias")
+        Z = tf.matmul(X, W) + b
+        if activation is not None:
+            return activation(Z)
+        else:
+            return Z
+
+with tf.name_scope("dnn"):
+    hidden1 = neuron_layer(X, n_hidden1, name="hidden1", activation=tf.nn.relu)
+    hidden2 = neuron_layer(hidden1, n_hidden2, name="hidden2", activation=tf.nn.relu)
+    logits = neuron_layer(hidden2, n_outputs, name="output")
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+learniing_rate = 0.01
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learniing_rate)
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    #tf.nn.in_top_k(predictions, targets, k)
+    #prediction中的每条记录是对M个目标类的响应,认为是正响应,响应越大,越有可能从属于某个类
+    #target是正确的类别标签在预测中的索引
+    #in_top_k 查看记录中的预测,前k个最大响应的下标集合是否包含target[i]
+    #返回的是boolean数组
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 40
+batch_size = 50
+
+def shuffle_batch(X, y, batch_size):
+    rnd_idx = np.random.permutation(len(X))
+    n_batches = len(X) // batch_size
+    for batch_idx in np.array_split(rnd_idx, n_batches):
+        X_batch, y_batch = X[batch_idx], y[batch_idx]
+        yield X_batch, y_batch
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run(training_op, feed_dict={X:X_batch, y:y_batch})
+        acc_batch = accuracy.eval(feed_dict={X:X_batch, y:y_batch})
+        acc_val = accuracy.eval(feed_dict={X:X_valid, y:y_valid})
+        print(epoch, "Batch accuracy:", acc_batch, "Val accuracy:", acc_val)
+    
+    save_path = saver.save(sess, "./my_model_final.ckpt")
