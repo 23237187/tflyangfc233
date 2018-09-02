fde6068538b1bbce52c5220a37d91d224c58e715
yang.fc <uestc2803305011@gmail.com>
2018-09-02 23:55:27 +0800

    20180902

---
+++ 11.py
@@ -0,0 +1,65 @@
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+# To plot pretty figures
+#%%matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "deep"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+#================================Vanishing/Exploding Gradients Problem==================================================
+def logit(z):
+    return 1 / (1 + np.exp(-z))
+
+z = np.linspace(-5, 5, 200)
+
+plt.plot([-5, 5], [0, 0], 'k-')
+plt.plot([-5, 5], [1, 1], 'k--')
+plt.plot([0, 0], [-0.2, 1.2], 'k-')
+plt.plot([-5, 5], [-3/4, 7/4], 'g--')
+plt.plot(z, logit(z), "b-", linewidth=2)
+props = dict(facecolor='black', shrink=0.1)
+plt.annotate('Saturating', xytext=(3.5, 0.7), xy=(5, 1), arrowprops=props, fontsize=14, ha="center")
+plt.annotate('Saturating', xytext=(-3.5, 0.3), xy=(-5, 0), arrowprops=props, fontsize=14, ha="center")
+plt.annotate('Linear', xytext=(2, 0.2), xy=(0, 0.5), arrowprops=props, fontsize=14, ha="center")
+plt.grid(True)
+plt.title("Sigmoid activation function", fontsize=14)
+plt.axis([-5, 5, -0.2, 1.2])
+
+save_fig("sigmoid_saturation_plot")
+plt.show()
+
+
+##=================================Xavier and He Initialization=========================================================
+import tensorflow as tf
+
+reset_graph()
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+
+he_init = tf.variance_scaling_initializer()
+hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu,
+                             kernel_initializer=he_init, name="hidden1")
