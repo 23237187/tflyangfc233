015e92f8c4c925287cd1da31fe8cfa56035d2264
yangfc <uestc2803305011@gmaill.com>
2018-09-04 16:41:44 +0800

    20180904

--- 11.py
+++ 11.py
@@ -63,3 +63,95 @@ X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
 he_init = tf.variance_scaling_initializer()
 hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu,
                              kernel_initializer=he_init, name="hidden1")
+
+#================================Nonsaturating Activation Functions==========================================
+##======================================Learky ReLU=========================================================
+def leaky_relu(z, alpha=0.01):
+    return np.maximum(alpha * z, z)
+
+plt.plot(z, leaky_relu(z, 0.05), "b-", linewidth=2)
+plt.plot([-5, 5], [0, 0], 'k-')
+plt.plot([0, 0], [-0.5, 4.2], 'k-')
+plt.grid(True)
+props = dict(facecolor='black', shrink=0.1)
+plt.annotate('Leak', xytext=(-3.5, 0.5), xy=(-5, -0.2), arrowprops=props, fontsize=14, ha="center")
+plt.title("Leaky ReLU activation function", fontsize=14)
+plt.axis([-5, 5, -0.5, 4.2])
+
+save_fig("leaky_relu_plot")
+plt.show()
+
+###===========================Implementing Leaky ReLU in TensorFlow:===================================
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+
+def leaky_relu(z, name=None):
+    return tf.maximum(0.01 * z, z, name=name)
+
+hidden1 = tf.layers.dense(X, n_hidden1, activation=leaky_relu, name="hidden1")
+
+###============================Train Leaky ReLU on MNIST==========================================
+reset_graph()
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+n_hidden2 = 100
+n_output = 10
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+with tf.name_scope("dnn"):
+    hidden1 = tf.layers.dense(X, n_hidden1, activation=leaky_relu, name="hidden1")
+    hidden2 = tf.layers.dense(hidden1, n_hidden2, activation=leaky_relu, name="hidden2")
+    logit = tf.layers.dense(hidden2, n_inputs, name="outputs")
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logit)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+learning_rate = 0.01
+
+with tf.name_scope("train"):
+     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+     training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logit, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+X_train = X_train.astype(np.float32).reshape(-1, 28*28) / 255.0
+X_test = X_test.astype(np.float32).reshape(-1, 28*28) / 255.0
+y_train = y_train.astype(np.int32)
+y_test = y_test.astype(np.int32)
+X_valid, X_train = X_train[:5000], X_train[5000:]
+y_valid, y_train = y_train[:5000], y_train[5000:]
+
+def shuffle_batch(X, y, batch_size):
+    rnd_idx = np.random.permutation(len(X))
+    n_batches = len(X) // batch_size
+    for batch_idx in np.split(rnd_idx, n_batches):
+        X_batch, y_batch = X[batch_idx], y[batch_idx]
+        yield X_batch, y_batch
+
+n_epochs = 40
+batch_size = 50
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        i = 0
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run(training_op, feed_dict={X: X_train, y: y_train})
+            print ("Epoch:", epoch, "Batch:", i)
+            i = i + 1
+        if epoch % 5 == 0:
+            acc_batch = accuracy.eval(feed_dict={X: X_train, y: y_train})
+            acc_valid = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+            print(epoch, "Batch accuracy:", acc_batch, "Validation accuracy:", acc_valid)
+    save_path = saver.save(sess, "./my_model_final.ckpt")
